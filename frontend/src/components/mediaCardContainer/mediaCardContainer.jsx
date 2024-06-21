8393d769cf5826e8e2bf1a04083ae977c236bf69
Yelhie <maelle.ew.m@gmail.com>
2024-06-21 22:11:10 +0200

    Maj Frontend : Mise en place du fonction sort pour le contenu des galeries
    - Ajout d'une fonction de tri par date, titre et popularité pour chaque média.

--- frontend/src/components/mediaCardContainer/mediaCardContainer.jsx
+++ frontend/src/components/mediaCardContainer/mediaCardContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchMediasByIdPhotographerThunk } from "../../redux/thunks/mediasThunks";
@@ -9,8 +9,7 @@ export const MediaCardContainer = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const medias = useSelector((state) => state.medias.medias);
-
-  // console.log(medias);
+  const [sortCriteria, setSortCriteria] = useState("date");
 
   useEffect(() => {
     if (id) {
@@ -18,10 +17,38 @@ export const MediaCardContainer = () => {
     }
   }, [dispatch, id]);
 
+  const getSortedMedias = () => {
+    return [...medias].sort((a, b) => {
+      switch (sortCriteria) {
+        case "likes":
+          return b.likes - a.likes;
+        case "name":
+          return a.title.localeCompare(b.title);
+        case "date":
+          return new Date(b.date) - new Date(a.date);
+        default:
+          return 0;
+      }
+    });
+  };
+
   return (
     <section className="section-photographer-gallery">
+      <div className="sort-selector">
+        <label htmlFor="sortCriteria">Trier par :</label>
+        <select
+          id="sortCriteria"
+          value={sortCriteria}
+          onChange={(event) => setSortCriteria(event.target.value)}
+          aria-label="Critères de tri"
+        >
+          <option value="date">Date</option>
+          <option value="likes">Popularité</option>
+          <option value="name">Nom</option>
+        </select>
+      </div>
       <div className="gallery">
-        {medias.map((media) => (
+        {getSortedMedias().map((media) => (
           <MediaCard key={media._id} media={media} />
         ))}
       </div>
